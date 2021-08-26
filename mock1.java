cc61f2a256e15d4e17d7010522e81c8474e60e23
Dania-Khan <61491029+Dania-Khan@users.noreply.github.com>
2021-08-27 01:32:35 +0530

    mock1.java

---
+++ mock1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int count = 0;
+    String inp = sc.next ();
+      inp = inp.replaceAll ("[aeiouAEIOU]", "#");
+    for (int i = 0; i < inp.length (); i++)
+      {
+	char c = inp.charAt (i);
+	if (c == '#')
+	  {
+	    count = count + 1;
+	  }
+
+      }
+    if (count > 0)
+      {
+	System.out.println (inp);
+      }
+    else
+      {
+	System.out.println ("NO VOWELS " + inp);
+
+      }
+
+  }
+}
